02d1fea6f100b7ecc68c4728f880186391763781
Valerii Boiko <valerii.boiko.1998@gmail.com>
2023-03-29 18:43:41 +0200

    setup path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          "@screens": "./src/screens",
+          "@navigation": "./src/navigation",
+          "root": "./src",
+        }
+      }
+    ]
+  ]
 };
