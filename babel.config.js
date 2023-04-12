3012040e757dc2396ce7cc9ff4de899ccf62c7f3
Valerii Boiko <valerii.boiko.1998@gmail.com>
2023-04-12 11:34:24 +0200

    add icon and pixel fonts

--- babel.config.js
+++ babel.config.js
@@ -7,13 +7,17 @@ module.exports = {
         root: ['.'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          "@screens": "./src/screens",
-          "@navigation": "./src/navigation",
-          "@theme": "./src/theme",
-          "root": "./src",
-        }
-      }
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@theme': './src/theme',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          root: './src',
+        },
+      },
     ],
     'react-native-reanimated/plugin',
-  ]
+  ],
 };
